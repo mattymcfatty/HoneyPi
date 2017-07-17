61488b2792ed8dd3b36b3dc337565be6c6eefd25
Matt South <matty.south@gmail.com>
2017-07-17 16:44:13 -0500

    Delete mystartup.sh
    Making a nice polished version

--- mystartup.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-# this forwards iptables for psad
-# drop this file in /etc/init.d
-# not 100% sure if this is necessary if iptables-persistent is installed
-
-iptables -A INPUT -j LOG
-iptables -A FORWARD -j LO
